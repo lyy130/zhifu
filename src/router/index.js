911b51390a4546c30409b310bb826ee7fede7c8c
lyy130 <1398857499@qq.com>
2022-03-30 16:29:41 +0800

    第二次提交

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/wall/recharge.vue',
+    path: '/wall/recharge',
     name: 'recharge',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
--- src/views/wall/recharge.vue
+++ src/views/wall/recharge.vue
@@ -2,58 +2,145 @@
   <div class="recharge">
      <div class="dliog">
          <img src="../../assets/images/pic_title.png@3x@3x.png" alt="" class="superFirst">
-         <p>选择充值金额</p>
+         <p class="selectP">选择充值金额</p>
          <div class="jeType">
-           <van-tabs v-model="active">
-              <van-tab title="视屏">
+           <van-tabs v-model="active" animated swipeable>
+              <van-tab title="￥298">
                 <ul class="jeTypeContent">
                   <li>
                     <a href="#">
-                      <img src="" alt="">
-                      <img src="" alt="">
+                      <img src="../../assets/images/bg@3x.png" alt="">
+                      <img src="../../assets/images/Vb@3x.png" alt="">
+                      <img src="../../assets/images/vbsmall@3x.png" alt="">
+                      <span>x900</span>
                     </a>
                   </li>
                   <li>
                     <a href="#">
-                      <img src="" alt="">
-                      <img src="" alt="">
+                      <img src="../../assets/images/bg@3x.png" alt="">
+                      <img src="../../assets/images/syc@3x.png" alt="">
+                      <img src="../../assets/images/sycsmall@3x.png" alt="">
+                      <span>x900</span>
                     </a>
                   </li>
                   <li>
                     <a href="#">
-                      <img src="" alt="">
-                      <img src="" alt="">
+                      <img src="../../assets/images/bg@3x.png" alt="">
+                      <img src="../../assets/images/pic_conversation.png@3x@3x.png" alt="">
+                      <img src="../../assets/images/pic_conversationsamll@3x.png" alt="">
+                      <span>x900</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                     <img src="../../assets/images/bg@3x.png" alt="">
+                     <img src="../../assets/images/vip30day@3x.png" alt="">
+                     <img src="../../assets/images/vip30daysmall@3x.png" alt="">
+                     <span>x900</span>
                     </a>
                   </li>
                 </ul>
               </van-tab>
-              <van-tab title="推荐">
-                <!-- <template #title>选项 </template> -->
-              </van-tab>
-              <van-tab title="新人">
+              <van-tab title="199">
                 <ul class="jeTypeContent">
                   <li>
                     <a href="#">
-                      <img src="" alt="">
-                      <img src="" alt="">
+                      <img src="../../assets/images/bg@3x.png" alt="">
+                      <img src="../../assets/images/Vb@3x.png" alt="">
+                      <img src="../../assets/images/vbsmall@3x.png" alt="">
+                      <span>x900</span>
                     </a>
                   </li>
                   <li>
                     <a href="#">
-                      <img src="" alt="">
-                      <img src="" alt="">
+                      <img src="../../assets/images/bg@3x.png" alt="">
+                      <img src="../../assets/images/syc@3x.png" alt="">
+                      <img src="../../assets/images/sycsmall@3x.png" alt="">
+                      <span>x900</span>
                     </a>
                   </li>
                   <li>
                     <a href="#">
-                      <img src="" alt="">
-                      <img src="" alt="">
+                     <img src="../../assets/images/bg@3x.png" alt="">
+                     <img src="../../assets/images/pic_conversation.png@3x@3x.png" alt="">
+                     <img src="../../assets/images/pic_conversationsamll@3x.png" alt="">
+                     <span>x900</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                     <img src="../../assets/images/bg@3x.png" alt="">
+                     <img src="../../assets/images/vip30day@3x.png" alt="">
+                     <img src="../../assets/images/vip30daysmall@3x.png" alt="">
+                     <span>x900</span>
+                    </a>
+                  </li>
+                </ul>
+              </van-tab>
+              <van-tab title="￥99">
+                <ul class="jeTypeContent">
+                  <li>
+                    <a href="#">
+                      <img src="../../assets/images/bg@3x.png" alt="">
+                      <img src="../../assets/images/Vb@3x.png" alt="">
+                      <img src="../../assets/images/vbsmall@3x.png" alt="">
+                      <span>x900</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <img src="../../assets/images/bg@3x.png" alt="">
+                      <img src="../../assets/images/syc@3x.png" alt="">
+                      <img src="../../assets/images/sycsmall@3x.png" alt="">
+                      <span>x900</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                     <img src="../../assets/images/bg@3x.png" alt="">
+                     <img src="../../assets/images/pic_conversation.png@3x@3x.png" alt="">
+                     <img src="../../assets/images/pic_conversationsamll@3x.png" alt="">
+                     <span>x900</span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                     <img src="../../assets/images/bg@3x.png" alt="">
+                     <img src="../../assets/images/vip30day@3x.png" alt="">
+                     <img src="../../assets/images/vip30daysmall@3x.png" alt="">
+                     <span>x900</span>
                     </a>
                   </li>
                 </ul>
               </van-tab>
             </van-tabs>
+           <div class="jeTypeBottom">
+             <img src="../../assets/images/dian_left@3x.png" alt=""/>
+             <p>首充<span>XXX元</span>可享价值<span>XXXX元</span>超值大礼包</p>
+             <img src="../../assets/images/dian_right@3x.png" alt=""/>
+           </div>
          </div>
+         <p class="selectP">选择支付方式</p>
+         <ul class="zfType" >
+           <li @click.capture="zfTypeClick($event,0)">
+             <img src="../../assets/images/pic_zfb.png@3x@3x.png" alt="" data-index="0">
+             <span>支付宝</span>
+             <img src="../../assets/images/btn_zf_highlightedl.png@3x@3x.png" alt="" v-show="select==0" >
+           </li>
+           <li @click.capture="zfTypeClick($event,1)">
+             <img src="../../assets/images/vb_weixin@3x@3x@3x.png" alt="" data-index="1">
+             <span>微信</span>
+             <img src="../../assets/images/btn_zf_highlightedl.png@3x@3x.png" alt="" v-show="select==1"  >
+           </li>
+           <li  @click.capture="zfTypeClick($event,2)">
+             <img src="../../assets/images/pic_pp.png@3x@3x.png" alt="" data-index="2">
+             <span>PayPel</span>
+             <img src="../../assets/images/btn_zf_highlightedl.png@3x@3x.png" alt="" v-show="select==2" >
+           </li>
+         </ul>
+        <div class="rightLq">
+          <img src="../../assets/images/btn_receive_normal.png@3x@3x.png" alt="">
+        </div>
      </div>
   </div>
 </template>
@@ -63,7 +150,13 @@
     name:'recharge',
     data(){
       return{
-          active:0
+          active:0,
+          select:0
+      }
+    },
+    methods:{
+      zfTypeClick(e,i){
+        this.select = i;
       }
     }
   }
@@ -71,22 +164,196 @@
 
 <style lang="less" scoped>
   .dliog{
-    width:7rem ;
-    height: 11.2rem;
+    width:7.5rem ;
+    height: calc(~'559.67/100*2rem');
     position: fixed;
     box-sizing: border-box;
-    top: 0.76rem;
-    left: 50%;
-    margin-left: -3.5rem;
-    background: #ccc;
+    padding: 0 calc(15/100*2rem);
+    top: 0.1rem;
+    // left: 50%;
+    // margin-left: -3.5rem;
+    background: url(../../assets/images/toast-recharge-bg.2f81add.png) no-repeat;
+    background-size: 100% 100%;
     img{
       width: 4.76rem;
       height: 1.69rem;
+      margin-top: 43px;
+    }
+    /deep/.van-tabs__line{
+      width: calc(~'105/100*2rem');
+      height: calc(~'44/100*2rem');
+      background: url(../../assets/images/action.png);
+      background-size: 100% 100%;
+      bottom: 11px;
     }
   }
+  .selectP{
+    font-size: calc(~'13/100*2rem');
+    color: #686868;
+    margin: 0;
+    margin-left: calc(~'14/100*2rem');
+  }
   /deep/.van-tabs__nav--line{
+    width: calc(~'311/100*2rem');
+    margin: 0 auto;
     background: url(../../assets/images/jeLan@3x.png) no-repeat;
     background-size: 100%;
   }
-  
+  .jeType{
+    background: url(../../assets/images/bg_mid@3x.png) no-repeat;
+    background-size: 94.3% 85%;
+    background-position: 10px 22px;
+    // width: calc(~'343.34/100*2rem');
+    width: 6.866rem;
+    margin: 0 auto;
+  }
+  /deep/.van-tab__text--ellipsis{
+    z-index: 1000;
+  }
+  /deep/.van-tab--active {
+      color: #fff;
+      font-size: 0.3rem;
+  }
+  /deep/.van-tabs__content--animated{
+    width: calc(~'343.34/100*2rem');
+    margin: 0 auto;
+    .van-tabs__track{
+      width: calc(~'343.34/100*2rem');
+    }
+  }
+  .jeTypeContent{
+    width: calc(~'311/100*2rem');
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 0px calc(~'10/100*2rem');
+    box-sizing: border-box;
+    margin: 0 auto;
+    margin-top: 5px;
+    overflow: hidden;
+    li{
+      position: relative;
+      width: calc(~'60/100*2rem');
+      height: calc(~'73.17/100*2rem');
+      a{
+        display: inline-block;
+        width: calc(~'60/100*2rem');
+        height: calc(~'73.17/100*2rem');
+        img{
+           position: absolute;
+           margin-top: 0;
+         &:nth-of-type(1){
+          width: calc(~'60/100*2rem');
+          height: calc(~'73.17/100*2rem');
+         }
+         &:nth-of-type(2){
+           top: 14px;
+           left: 16px;
+          width: calc(~'30/100*2rem');
+          height: calc(~'30.33/100*2rem');
+         }
+         &:nth-of-type(3){
+           width: calc(~'36.54/100*2rem');
+           height: calc(~'11.54/100*2rem');
+            top: 0;
+            right: 0;
+         }
+        }
+        span{
+          position: absolute;
+          left: 50%;
+          margin-left: -15px;
+           bottom: 0;
+           font-size: calc(~'12/100*2rem');
+           color: #FD7D00;
+        }
+      }
+    }
+  }
+  .jeTypeBottom{
+    width: calc(~'311/100*2rem');
+    height: calc(~'57.67/100*2rem');
+    background: url(../../assets/images/pic_d.png@3x@3x.png);
+    background-size: 100% 100%;
+    position: relative;
+    margin: 0 auto;
+    img{
+      width: calc(~'18/100*2rem');
+      height: calc(~'4/100*2rem');
+      position: absolute;
+      &:nth-of-type(1){
+        top: 50%;
+        left: 13px;
+        margin-top: -6px;
+      }
+      &:nth-of-type(2){
+        top: 50%;
+        right: 13px;
+        margin-top: -6px;
+      }
+    }
+    p{
+      color: white;
+      font-size: calc(~'13.5/100*2rem');
+      padding: 0 35px;
+      box-sizing: border-box;
+      line-height: calc(~'52/100*2rem');
+      span{
+        color: #FF7100;
+      }
+    }
+  }
+  .zfType{
+    width: calc(~'311/100*2rem');
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    li{
+      position: relative;
+      margin-top: 16px;
+      width: calc(~'124/100*2rem');
+      height: calc(~'44/100*2rem');
+      line-height: calc(~'35/100*2rem');
+      padding-left: 10px;
+      &:nth-of-type(2){
+        margin-left: calc(~'38/100*2rem');
+      }
+      &:nth-of-type(3){
+        margin-top: 24px;
+      }
+      img{
+        width: calc(~'24/100*2rem');
+        height: calc(~'24/100*2rem');
+        margin-top: 0px;
+        &:nth-of-type(2){
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: calc(~'124/100*2rem');
+          height: calc(~'44/100*2rem');
+          z-index: -1;
+        }
+      }
+      span{
+        display: inline-block;
+        font-size:calc(~'13/100*2rem') ;
+        color: #FD7D00;
+        vertical-align: middle;
+        margin-left: 10px;
+        margin-top: 2px;
+      }
+    }
+  }
+  .rightLq{
+    width: calc(~'302/100*2rem');
+    height: calc(~'50.66/100*2rem');
+    margin: 0 auto;
+    margin-top: 3px;
+    img{
+      width: 100%;
+      height: 100%;
+      margin-top: 0;
+    }
+  }
+
 </style>
